d45ad01db44ff59bba8ddc2c4dbd7b4c1a77b303
slvtn <jerenkotomi@gmail.com>
2022-04-12 13:25:08 +0200

    srvr

--- internal/loadbalancer.go
+++ internal/loadbalancer.go
@@ -1,7 +1,35 @@
 package loadbalancer
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"net/http/httputil"
+)
 
-func Start() {
-	fmt.Println("Hello")
+var tmp bool = false
+
+func balance(request *http.Request) {
+	request.URL.Scheme = "http"
+	request.URL.Host = ":8081"
+
+	if tmp {
+		request.URL.Path = "/left"
+		tmp = false
+	} else {
+		request.URL.Path = "/right"
+		tmp = true
+	}
+}
+
+func Start(address string) {
+	reverseProxy := &httputil.ReverseProxy{
+		Director: balance,
+	}
+	server := http.Server{
+		Addr:    address,
+		Handler: reverseProxy,
+	}
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err.Error())
+	}
 }
